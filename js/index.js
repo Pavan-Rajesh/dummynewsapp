20c28b5a41763aea82124f4a600ac954703c8453
unknown <pavanrajesh365@gmail.com>
2023-01-09 21:08:44 +0530

    added hyperlinks

--- js/index.js
+++ js/index.js
@@ -47,3 +47,38 @@ function thumbsdown(x) {
         v.style.filter = 'grayscale(0%)'
     }
 }
+
+
+const search = document.getElementById("search")
+search.addEventListener('click', function () {
+    const searchbar = document.getElementById('search-bar');
+    if (searchbar.style.display == 'none') {
+        searchbar.style.display = 'block';
+    } else {
+        searchbar.style.display = 'none';
+    }
+})
+
+
+function google() {
+    const s = document.getElementById('search-text');
+    console.log(s.value);
+    location.href = 'http://www.google.com/search?q=' + s.value;
+}
+
+const searchmin = document.getElementById("search-min")
+searchmin.addEventListener('click', function () {
+    const searchbarmin = document.getElementById('search-bar-min');
+    if (searchbarmin.style.display == 'none') {
+        searchbarmin.style.display = 'block';
+    } else {
+        searchbarmin.style.display = 'none';
+    }
+})
+
+
+function google() {
+    const s = document.getElementById('search-text');
+    console.log(s.value);
+    location.href = 'http://www.google.com/search?q=' + s.value;
+}
